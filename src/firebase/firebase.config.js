c9fcac81cb3ebf4a5ee95b193382cf1e829ef618
tabib-e-alahi <etabib.alahi@gmail.com>
2023-10-04 01:27:11 +0600

    after env setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdvSm0NKfxB4T2Vveu-JrpnUSVST6Qy6Q",
-  authDomain: "react-dragon-news-auth-86837.firebaseapp.com",
-  projectId: "react-dragon-news-auth-86837",
-  storageBucket: "react-dragon-news-auth-86837.appspot.com",
-  messagingSenderId: "801565153862",
-  appId: "1:801565153862:web:3bbe7a9e0cf866e39a4847"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
